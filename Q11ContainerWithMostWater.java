87b23bd78bd208f76469fef71a367bd57049fdb2
SPN288 <spnirala288@gmail.com>
2024-07-09 17:53:34 +0530

    day26

--- Q11ContainerWithMostWater.java
+++ Q11ContainerWithMostWater.java
@@ -1,19 +1,19 @@
 public class Q11ContainerWithMostWater {
     
 
-    public static int maxArea(int[] height) {
-        int s=height.length;
-        if(s==0||s==1){return 0;}
-        int ans=0;
-        int n=0;
-        for (int i = 0; i <s; i++) {
-            for(int j=0;j<s;j++){
-                int w=Math.min(height[i], height[j])*(j-i);
-                if(w>ans){ans=w;}
-            }
-        }
-        return ans;
-    }
+    // public static int maxArea(int[] height) {
+    //     int s=height.length;
+    //     if(s==0||s==1){return 0;}
+    //     int ans=0;
+    //     int n=0;
+    //     for (int i = 0; i <s; i++) {
+    //         for(int j=0;j<s;j++){
+    //             int w=Math.min(height[i], height[j])*(j-i);
+    //             if(w>ans){ans=w;}
+    //         }
+    //     }
+    //     return ans;
+    // }
 
     // public static int maxArea(int[] height) {
     //     int s=height.length;
@@ -23,7 +23,7 @@ public class Q11ContainerWithMostWater {
     //     int j=s-1;
     //     int maxf=0;
     //     int maxr=j;
-    //     for ( i = 0; i < s; i++) {
+    //     for ( i = 0; i < s;i++) {
     //         if(height[i]>height[maxf]){
     //             maxf=i;
     //         }
@@ -32,10 +32,28 @@ public class Q11ContainerWithMostWater {
     //         }
     //         int w=Math.min(height[maxf], height[maxr])*(maxr-maxf);
     //              if(w>ans){ans=w;}
-    //              j--;
+    //             j--;
     //     }
     //     return ans;
     // }
+    
+        public static  int maxArea(int[] height) {
+          int ans = 0;
+          int l = 0;
+          int r = height.length - 1;
+      
+          while (l < r) {
+            final int minHeight = Math.min(height[l], height[r]);
+            ans = Math.max(ans, minHeight * (r - l));
+            if (height[l] < height[r])
+              ++l;
+            else
+              --r;
+          }
+      
+          return ans;
+        }
+      
 
     public static void main(String[] args) {
         int[] arr1={1,8,6,2,5,4,8,3,7};//49
