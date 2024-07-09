265c24eb4324e58c75f6104ef80ac6fb6dab4cb7
SPN288 <spnirala288@gmail.com>
2024-07-09 17:23:42 +0530

    day26

--- Q11ContainerWithMostWater.java
+++ Q11ContainerWithMostWater.java
@@ -2,11 +2,12 @@ public class Q11ContainerWithMostWater {
     
 
     public static int maxArea(int[] height) {
-        if(height.length==0||height.length==1){return 0;}
+        int s=height.length;
+        if(s==0||s==1){return 0;}
         int ans=0;
         int n=0;
-        for (int i = 0; i < height.length; i++) {
-            for(int j=0;j<height.length;j++){
+        for (int i = 0; i <s; i++) {
+            for(int j=0;j<s;j++){
                 int w=Math.min(height[i], height[j])*(j-i);
                 if(w>ans){ans=w;}
             }
@@ -14,9 +15,31 @@ public class Q11ContainerWithMostWater {
         return ans;
     }
 
+    // public static int maxArea(int[] height) {
+    //     int s=height.length;
+    //     if(s==0||s==1){return 0;}
+    //     int ans=0;
+    //     int i;
+    //     int j=s-1;
+    //     int maxf=0;
+    //     int maxr=j;
+    //     for ( i = 0; i < s; i++) {
+    //         if(height[i]>height[maxf]){
+    //             maxf=i;
+    //         }
+    //         if(height[j]>height[maxr]){
+    //             maxr=j;
+    //         }
+    //         int w=Math.min(height[maxf], height[maxr])*(maxr-maxf);
+    //              if(w>ans){ans=w;}
+    //              j--;
+    //     }
+    //     return ans;
+    // }
+
     public static void main(String[] args) {
         int[] arr1={1,8,6,2,5,4,8,3,7};//49
-        int[] arr2 ={1,1};//1   
+        int[] arr2 ={2,3,4,5,18,17,6};//17  
         System.out.println(maxArea(arr1));
         System.out.println(maxArea(arr2));
     }
