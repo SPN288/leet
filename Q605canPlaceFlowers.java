c7365546daba71e79ba8f184a782d1444c6c01d1
SPN288 <spnirala288@gmail.com>
2024-08-09 13:19:15 +0530

    day56

--- Q605canPlaceFlowers.java
+++ Q605canPlaceFlowers.java
@@ -5,31 +5,35 @@ public class Q605canPlaceFlowers {
         if (flowerbed.length == 0 && n > 0) {
             return false;
         }
-        if(flowerbed.length==1&&flowerbed[0]==0){
+        if (flowerbed.length == 1 && flowerbed[0] == 0) {
             return true;
-        }
-        else if(flowerbed.length==1&&(flowerbed[0]==1&&n>0)){
+        } else if (flowerbed.length == 1 && (flowerbed[0] == 1 && n > 0)) {
             return false;
         }
-        
+
         for (int i = 0; i < flowerbed.length; i++) {
-            if (i == 0 && (flowerbed[i] == 0 && flowerbed[i + 1] == 0)) {
+            if (i == 0) {
+                if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                    flowerbed[0] = 1;
+                    n--;
+                }
+                continue;
+
+            }
+            if (i == (flowerbed.length-1)) {
+                if(flowerbed[i] == 0 && flowerbed[i - 1] == 0){
                 flowerbed[i] = 1;
                 n--;
+                }
                 continue;
+
             }
-            if (i == (flowerbed.length-1) && (flowerbed[i] == 0 && flowerbed[i - 1] == 0)) {
-                flowerbed[i] = 1;
-                n--;
-                continue;
-            }
-            
             if (flowerbed[i] == 0 && (flowerbed[i - 1] != 1 && flowerbed[i + 1] != 1)) {
                 flowerbed[i] = 1;
                 n--;
             }
         }
-        
+
         if (n <= 0) {
             return true;
         } else {
@@ -40,9 +44,9 @@ public class Q605canPlaceFlowers {
     public static void main(String[] args) {
         int[] arr = {1, 0, 0, 0, 1, 0, 0};
         int[] arr1 = {1, 0, 0, 0, 0, 1};
-        System.out.println(canPlaceFlowers(arr, 2));
-
+        int[] arr2 = {0, 1, 0};
         System.out.println(canPlaceFlowers(arr, 2));
         System.out.println(canPlaceFlowers(arr1, 2));
+        System.out.println(canPlaceFlowers(arr2, 1));
     }
 }
