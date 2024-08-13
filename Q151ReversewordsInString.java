1486d722497d424dcd08845e497ef1a585aae199
SPN288 <spnirala288@gmail.com>
2024-08-13 09:55:30 +0530

    day59

--- Q151ReversewordsInString.java
+++ Q151ReversewordsInString.java
@@ -1,29 +1,36 @@
+
 public class Q151ReversewordsInString {
+
     public static String reverseWords(String s) {
-        StringBuilder ans=new StringBuilder();
-        StringBuilder temp=new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
 
-        
-        for (int i = s.length()-1; i >=0; i--) {
-            
-            if(s.charAt(i)==' '){
-                ans.append(temp.reverse()+" ");
-                temp.delete(0,ans.length());
-                i--;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                temp.append(s.charAt(i));
             }
-            temp.append(s.charAt(i));
-            if(i==0){ans.append(temp.reverse());}
+            if (s.charAt(i) == ' ' && temp.length() != 0) {
+                ans.append(temp.reverse() + " ");
+                temp.delete(0, temp.length());
+            }
 
+            if (i == 0) {
+                ans.append(temp.reverse());
+            }
+            
         }
-
-
+        
+        if(ans.charAt(ans.length()-1)==' '){
+            ans.deleteCharAt(ans.length()-1);
+        }
         return ans.toString();
     }
+
     public static void main(String[] args) {
-        System.out.println(reverseWords("the sky is blue"));
-        
-        System.out.println(reverseWords("  hello world  "));
-        
-        System.out.println(reverseWords("a good   example"));
+        System.out.println("START->"+reverseWords("the sky is blue")+"<-END");
+
+        System.out.println("START->"+reverseWords("  hello world  ")+"<-END");
+
+        System.out.println("START->"+reverseWords("a good   example")+"<-END");
     }
 }
