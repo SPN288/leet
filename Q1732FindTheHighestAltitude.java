3858e18855a683398811f0aacae4232295211bae
SPN288 <spnirala288@gmail.com>
2024-08-15 13:47:45 +0530

    day61

--- Q1732FindTheHighestAltitude.java
+++ Q1732FindTheHighestAltitude.java
@@ -1,27 +1,28 @@
+
 public class Q1732FindTheHighestAltitude {
+
     public static int largestAltitude(int[] gain) {
-        int ans=0;
-        int[] height=new int[gain.length+1];
-        height[0]=0;
-        int max=Integer.MIN_VALUE;
-        int i=0;
-        for ( i = 0; i < gain.length; i++) {
-            ans+=gain[i];
-            height[i+1]=ans;
+        int ans = 0;
+        int l = gain.length;
+        int[] height = new int[l + 1];
+        height[0] = 0;
+        int max = Integer.MIN_VALUE;
+        int i = 0;
+        for (i = 0; i < l; i++) {
+            ans += gain[i];
+            height[i + 1] = ans;
         }
-        for ( i = 0; i < height.length; i++) {
-            if(height[i]>max){
-                max=height[i];
+        for (i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
             }
         }
-
-
 
         return max;
     }
 
     public static void main(String[] args) {
-        int []arr={-5,1,5,0,-7};
+        int[] arr = {-5, 1, 5, 0, -7};
         System.out.println(largestAltitude(arr));
     }
 }
