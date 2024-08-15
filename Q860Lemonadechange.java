dc343c51c7b2dba2548b849326805352bf78da62
SPN288 <spnirala288@gmail.com>
2024-08-15 20:46:43 +0530

    day61

---
+++ Q860Lemonadechange.java
@@ -0,0 +1,38 @@
+
+public class Q860Lemonadechange {
+
+    public static boolean lemonadeChange(int[] bills) {
+
+        int five = 0, ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                five++;
+            }
+            if (bills[i] == 10) {
+                if (five > 0) {
+                    ten++;
+                    five--;
+                } else {
+                    return false;
+                }
+            }
+            if (bills[i] == 20) {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if(five>=3){
+                    five-=3;
+                }else{return false;}
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5};
+        int[] bills2 = {5, 5, 10, 10, 20};
+        System.out.println(lemonadeChange(bills));
+        System.out.println(lemonadeChange(bills2));
+    }
+}
