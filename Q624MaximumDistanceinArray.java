add15f31ed975c1fbb0708d27126f5d646ff2efd
SPN288 <spnirala288@gmail.com>
2024-08-16 23:36:18 +0530

    day62

--- Q624MaximumDistanceinArray.java
+++ Q624MaximumDistanceinArray.java
@@ -1,6 +1,12 @@
 
 import java.util.ArrayList;
 import java.util.List;
+/*You are given m arrays, where each array is sorted in ascending order.
+
+You can pick up two integers from two different arrays (each array picks one) and calculate the distance. 
+We define the distance between two integers a and b to be their absolute difference |a - b|.
+
+Return the maximum distance. */
 
 public class Q624MaximumDistanceinArray {
 
