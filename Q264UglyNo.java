12a34b2e4cd0704ef7c786665a37b0824c03d8b0
SPN288 <spnirala288@gmail.com>
2024-08-19 11:31:07 +0530

    day64

--- Q264UglyNo.java
+++ Q264UglyNo.java
@@ -13,6 +13,54 @@ public class Q264UglyNo {
         return true;
     }
 
+    // public static int nthUglyNumber(int n) {
+    //     if (n == 1) {
+    //         return 1;
+    //     }
+    //     if (n == 2) {
+    //         return 2;
+    //     }
+    //     if (n == 3) {
+    //         return 3;
+    //     }
+    //     int[] arr = new int[n];
+    //     arr[0] = 1;
+    //     arr[1] = 2;
+    //     arr[2] = 3;
+    //     int k = 3;
+    //     int i=4;
+    //     HashMap<Integer, Integer> hm = new HashMap<>();
+    //     hm.put(2, 2);
+    //     hm.put(3, 3);
+    //     hm.put(5, 5);
+
+    //     while (k<n) {
+    //         if (isPrime(i)&&i>5) {
+    //             i++;
+    //             continue;
+    //         }
+    //         boolean check = true;
+    //         for (int j = 2; j <= i / 2; j++) {
+    //             if (isPrime(j) && i % j == 0) {
+    //                 if (hm.containsKey(j)) {
+    //                     continue;
+    //                 } else {
+    //                     check = false;
+    //                 }
+    //             }
+    //         }
+    //         if (check == true) {
+    //             arr[k] = i;
+    //             k++;
+    //         }i++;
+    //     }
+    //     for ( i = 0; i < n; i++) {
+    //         System.out.print(arr[i] + " ");
+    //     }
+    //     System.out.println();
+
+    //     return arr[n - 1];
+    // }
     public static int nthUglyNumber(int n) {
         if (n == 1) {
             return 1;
@@ -23,10 +71,7 @@ public class Q264UglyNo {
         if (n == 3) {
             return 3;
         }
-        int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
+        int ans=0;
         int k = 3;
         int i=4;
         HashMap<Integer, Integer> hm = new HashMap<>();
@@ -50,18 +95,14 @@ public class Q264UglyNo {
                 }
             }
             if (check == true) {
-                arr[k] = i;
+                ans = i;
                 k++;
             }i++;
         }
-        for ( i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        
 
-        return arr[n - 1];
+        return ans;
     }
-
     public static void main(String[] args) {
         System.out.println(nthUglyNumber(124));
         //System.out.println(isPrime(4));
