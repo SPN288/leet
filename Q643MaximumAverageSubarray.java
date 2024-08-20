8046f373543fcbfdfe04a54fc4d260227a853772
SPN288 <spnirala288@gmail.com>
2024-08-20 20:10:32 +0530

    day65

--- Q643MaximumAverageSubarray.java
+++ Q643MaximumAverageSubarray.java
@@ -1,4 +1,6 @@
+
 public class Q643MaximumAverageSubarray {
+
     // public static double findMaxAverage(int[] nums, int k) {
     //     double max=Integer.MIN_VALUE;
     //     for (int i = 0; i <= nums.length-k; i++) {
@@ -13,27 +15,32 @@ public class Q643MaximumAverageSubarray {
     //     return max;
     // }
     public static double findMaxAverage(int[] nums, int k) {
-        int l=nums.length;
-        if(l==1){return nums[0];}
-        int i;
-        for ( i = 1; i <l; i++) {
-            nums[i]=nums[i-1]+nums[i];
+        int l = nums.length;
+        if (l == 1) {
+            return nums[0];
         }
-        double max=nums[k-1];
-        for(i=k;i<l;i++){     
-            if(nums[i]-nums[i-k]>max){
-                max=nums[i]-nums[i-k];
+        int i;
+        for (i = 1; i < l; i++) {
+            nums[i] = nums[i - 1] + nums[i];
+            
+        }
+
+        double max = nums[k-1];
+        for (i = k; i < l; i++) {
+            if (nums[i] - nums[i - k] > max) {
+                max = nums[i] - nums[i - k];
             }
         }
-        max=max/k;
+        max = max / k;
 
         return max;
     }
+
     public static void main(String[] args) {
-        int[] arr={4,0,4,3,3};
-        int[] arr2={9,7,3,5,6,2,0,8,1,9};
+        int[] arr = {4, 0, 4, 3, 3};
+        int[] arr2 = {9, 7, 3, 5, 6, 2, 0, 8, 1, 9};
         //System.out.println(findMaxAverage(arr, 5));
         System.out.println(findMaxAverage(arr2, 6));
     }
-    
+
 }
