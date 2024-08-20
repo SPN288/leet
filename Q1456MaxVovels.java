ceed20094ea3f198b29c7bf49320752e3f394fb7
SPN288 <spnirala288@gmail.com>
2024-08-20 21:45:11 +0530

    day65

--- Q1456MaxVovels.java
+++ Q1456MaxVovels.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Q1456MaxVovels {
@@ -25,10 +24,33 @@ public class Q1456MaxVovels {
             }
         }
 
-
-
         return max;
     }
+    // public static int maxVowels(String s, int k) {
+
+    //     int count = 0;
+    //     int i;
+    //     for (i = 0; i < k; i++) {
+    //         if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+    //             count++;
+    //         }
+    //     }
+    //     int max = count;
+    //     for (i = k; i < s.length(); i++) {
+    //         if (s.charAt(i - k) == 'a' || s.charAt(i - k) == 'e' || s.charAt(i - k) == 'i' || s.charAt(i - k) == 'o' || s.charAt(i - k) == 'u') {
+    //             count--;
+    //         }
+    //         if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+    //             count++;
+    //         }
+    //         if (count >= max) {
+    //             max = count;
+    //         }
+    //     }
+
+    //     return max;
+    // }
+
     public static void main(String[] args) {
         System.out.println(maxVowels("abciiidef", 3));
     }
