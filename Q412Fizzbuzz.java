41350896179bb6d9853de5576011fb44751c368d
SPN288 <spnirala288@gmail.com>
2024-09-01 20:25:41 +0530

    day

--- Q412Fizzbuzz.java
+++ Q412Fizzbuzz.java
@@ -1,24 +1,29 @@
+
 import java.util.*;
+
 public class Q412FizzBuzz {
-    class Solution {
-        public List<String> fizzBuzz(int n) {
-            List <String> ans=new ArrayList<>();
-            for(int i=1;i<=n;i++){
-                if(i%3==0&&i%5==0){
-                    ans.add("FizzBuzz");
-                    continue;
-                }
-                if(i%3==0){
-                    ans.add("Fizz");
-                    continue;
-                }
-                if(i%5==0){
-                    ans.add("Buzz");
-                    continue;
-                }
-                ans.add(String.valueOf(i));
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.add("FizzBuzz");
+                continue;
             }
-            return ans;
+            if (i % 3 == 0) {
+                ans.add("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                ans.add("Buzz");
+                continue;
+            }
+            ans.add(String.valueOf(i));
         }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(15));
     }
 }
