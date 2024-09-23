f84c81828b46ffd7ecba3ea39b4a8192ab7fd561
SPN288 <spnirala288@gmail.com>
2024-09-23 10:37:31 +0530

    day

--- countVovConSpace.java
+++ countVovConSpace.java
@@ -14,24 +14,32 @@ Consonants: 11
 White spaces: 4
  */
 public class countVovConSpace {
-    static void count(String s){
-        int v=0,c=0,sp=0;
-        HashMap<Character,Integer>hm=new HashMap();
+
+    static void count(String s) {
+        int v = 0, c = 0, sp = 0;
+        HashMap<Character, Integer> hm = new HashMap();
         hm.put('a', 1);
-        hm.put('e',2 );
-        hm.put('i',3 );hm.put('o',4 );hm.put('u',5 );
+        hm.put('e', 2);
+        hm.put('i', 3);
+        hm.put('o', 4);
+        hm.put('u', 5);
 
         for (int i = 0; i < s.length(); i++) {
-            char m=s.charAt(i);
-            if((m>='A'&&m<='Z')||(m>='a'&&m<='z')){
-                if(hm.containsKey(m)){
+            char m = s.charAt(i);
+            if ((m >= 'A' && m <= 'Z') || (m >= 'a' && m <= 'z')) {
+                if (hm.containsKey(m)) {
                     v++;
-                }else{c++;}
-            }else if(m==' '){sp++;}
+                } else {
+                    c++;
+                }
+            } else if (m == ' ') {
+                sp++;
+            }
         }
-        System.out.println("Vovels="+v+"Consonents="+c+"Space="+sp);
+        System.out.println("Vovels=" + v + "Consonents=" + c + "Space=" + sp);
 
     }
+
     public static void main(String[] args) {
         count("Satya Prakash Ghost");
     }
