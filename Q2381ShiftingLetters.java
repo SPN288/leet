2041d5096c14e5797ec199ba5e95bfdf300cd62b
SPN288 <spnirala288@gmail.com>
2025-01-05 09:41:11 +0530

    day

--- Q2381ShiftingLetters.java
+++ Q2381ShiftingLetters.java
@@ -1,41 +1,34 @@
 public class Q2381ShiftingLetters {
 
-    public static StringBuilder shift(StringBuilder s,int st,int ed,int d){
-        StringBuilder sb=new StringBuilder();
-        for (int i = 0; i < st; i++) {
-            sb.append(s.charAt(i));
-        }
-        for (int i = st; i <=ed; i++) {
-            char x=s.charAt(i);
-            if(d==0){
-                if(x=='a'){
-                    sb.append('z');
-                }else{
-                    x--;
-                    sb.append(x);
-                }
-            }else{
-                if(x=='z'){
-                    sb.append('a');
-                }else{
-                    x++;
-                    sb.append(x);
-                }
-            }
-        }
-        for (int i = ed+1; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-        }
-
-        return sb;
-    }
+    
     
     public static String shiftingLetters(String s, int[][] shifts) {
         StringBuilder sb=new StringBuilder(s);
-
+        
         for (int i = 0; i < shifts.length; i++) {
-            sb=shift(sb, shifts[i][0], shifts[i][1], shifts[i][2]);
-            System.out.println(sb);
+            for (int j = shifts[i][0]; j <=shifts[i][1]; j++) {
+                char x=sb.charAt(j);
+                if(shifts[i][2]==0){
+                    if(x=='a'){
+                        
+                        sb.setCharAt(j, 'z');
+                    }else{
+                        x--;
+                        
+                        sb.setCharAt(j, x);
+                    }
+                }else{
+                    if(x=='z'){
+                        
+                        sb.setCharAt(j, 'a');
+                    }else{
+                        x++;
+                        
+                        sb.setCharAt(j, x);
+                    }
+                }
+            }
+            //System.out.println(sb);
         }
 
         return sb.toString();
